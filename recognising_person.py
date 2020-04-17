5eeb00e1323510cd6dec90fadb97b9822ea8795c
himanshigupta10 <47886389+himanshigupta10@users.noreply.github.com>
2020-04-17 21:35:12 +0530

    Add files via upload

---
+++ recognising_person.py
@@ -0,0 +1,46 @@
+import cv2
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+# herte u can also use nose and mouth classifier too
+
+def generate_dataset(img, id, img_id):
+    cv2.imwrite("data/user."+str(id)+"."+str(img_id)+".jpg",img)
+
+def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, text, clf):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    features = classifier.detectMultiScale(gray_img, scaleFactor, minNeighbors)
+    coords = []
+    for (x, y, w, h) in features:
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        id, _ = clf.predict(gray_img[y:y+h, x:x+w])
+        if id == 1:
+        cv2.putText(img, text, (x, y-4), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 1, cv2.LINE_AA)
+        coords = [x, y, w, h]
+
+    return coords
+
+def detect(img, faceCascade, eyeCascade):
+    color = {"blue":(255, 0, 0), "red":(0, 0, 255), "green":(0, 255, 0)}
+    coords = draw_boundary(img, faceCascade, 1.1, 10, color['blue'], "Face")
+
+    if len(coords)==4:
+        roi_img = img[coords[1]:coords[1]+coords[3], coords[0]:coords[0]+coords[2]]
+        coords = draw_boundary(roi_img, eyeCascade, 1.1, 14, color['red'], "Eyes")
+
+    return img
+
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    _, img = video_capture.read()
+    img = detect(img, faceCascade, eyeCascade)
+    cv2.imshow('face detection', img)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+
+
+
